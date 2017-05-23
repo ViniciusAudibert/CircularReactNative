cee7c5a8efe8ead987671967eba18b3c670fb6fe
Leonardo Almeida <leonardo.almeida@cwi.com.br>
2017-05-23 14:34:44 -0300

    No input do campo de pesquisa adicionar o formato desejado para pesquisa

--- src/components/clock.bar.js
+++ src/components/clock.bar.js
@@ -1,12 +1,7 @@
-import React, {
-    Component
-} from 'react';
-import {
-    View,
-    Text,
-    TextInput,
-    StyleSheet
-} from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { TextInputMask } from 'react-native-masked-text';
+
 import CircularCtrl from '../controllers/circular.info.controller';
 
 class ClockBar extends Component {
@@ -27,31 +22,24 @@ class ClockBar extends Component {
     }
 
     render() {
-        return ( < TextInput style = {
-                styles.clockInput
-            }
-            onChangeText = {
-                (text) => {
-                    this.sortList(text);
-                }
-            }
-            value = {
-                this.state.text
-            }
-            placeholder = "Digite o horário da preferência"
-            autoFocus = {
-                false
-            }
-            />);
-        }
+        return (<TextInputMask style={styles.clockInput}
+            onChangeText={(text) => { this.sortList(text); }}
+            value={this.state.text}
+            placeholder="Digite o horário da preferência"
+            autoFocus={false}
+            type={'datetime'}
+            options={{ format: 'HH:mm' }}
+        />);
     }
+}
 
-    const styles = StyleSheet.create({
-        clockInput: {
-            height: 50,
-            borderColor: 'gray',
-            fontSize: 18
-        }
-    });
+const styles = StyleSheet.create({
+    clockInput: {
+        height: 50,
+        borderColor: 'gray',
+        fontSize: 18,
+        textAlign: 'center'
+    }
+});
 
-    export default ClockBar;
+export default ClockBar;
